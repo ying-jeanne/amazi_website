942b93f7d2ea446538dc6270c94e0f10fe057b9e
ying-jeanne <wangyingensea@gmail.com>
2025-12-28 13:05:15 +0200

    optimise images and backup originals

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.after_request
+def add_header(response):
+    # Cache static assets for 1 year (31536000 seconds)
+    if request.path.startswith('/static'):
+        response.headers['Cache-Control'] = 'public, max-age=31536000'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
